89fa82debd54274fd9520275a4f5b96e700e77a0
wang.yubo <wang.yubo@dragon.jp>
2017-05-23 18:43:40 +0900

    ログ収集シェルのサンプルです。

---
+++ shell/log_collect.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# MIT License
+# Author: wang.yubo@dragon.jp
+# Add this script to cron like bellow
+# 0 0 * * * /usr/bin/bash /your/script/save/space/log_collect.sh > /dev/null 2>&1
+
+# workspace
+WORK_DIR="/your/website/log/save/space/"
+
+# log export path
+LOG_DIR="${WORK_DIR}collect_logs_day/"
+
+# param　date format YYYY-MM-DD
+LOG_DATE=`date --date "1 day ago" "+%Y-%m-%d"`
+#LOG_DATE=$1
+
+# log file name
+LOGFILE="${LOG_DIR}${LOG_DATE}.log"
+
+#create log folder is not exist
+if [ ! -e $LOG_DIR ]; then mkdir -m 777 -p $LOG_DIR; fi
+
+#collect yesterday log
+#begin
+#collect access log
+echo "${LOG_DATE}'s access log start" >> $LOGFILE
+less ${WORK_DIR}access.log | grep $LOG_DATE >> $LOGFILE
+echo "${LOG_DATE}'s access log end" >> $LOGFILE
+
+#collect error log
+echo "${LOG_DATE}'s error log start" >> $LOGFILE
+less ${WORK_DIR}error.log | grep $LOG_DATE >> $LOGFILE
+echo "${LOG_DATE}'s error log end" >> $LOGFILE
+
+#collect debug log
+echo "${LOG_DATE}'s debug log start" >> $LOGFILE
+less ${WORK_DIR}debug.log | grep $LOG_DATE >> $LOGFILE
+echo "${LOG_DATE}'s debug log end" >> $LOGFILE
+#end
